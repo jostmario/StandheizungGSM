4f92f13e4b1c5b303f5cd366697eb24b4c8d5ed5
Jost Mario <reisbrettstift@gmx.de>
2019-11-06 06:35:03 +0100

    Project Start

---
+++ StandheizungGSM/StandheizungGSM/StandheizungGSM.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		StandheizungGSM.ino
+ Created:	06.11.2019 06:33:29
+ Author:	jost
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+	// Test
+
+}
