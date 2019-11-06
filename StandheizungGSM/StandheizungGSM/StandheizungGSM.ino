be781b66c71a1225ea34e7d757557adff361c54b
Jost Mario <reisbrettstift@gmx.de>
2019-11-06 21:10:15 +0100

    serielle Komuni verbessert

--- StandheizungGSM/StandheizungGSM/StandheizungGSM.ino
+++ StandheizungGSM/StandheizungGSM/StandheizungGSM.ino
@@ -4,19 +4,24 @@
  Author:	jost Mario
 */
 
-
 #define Sim900_Serial Serial1
 #define GpsNeo6_Serial Serial2
 
-
-static const uint32_t SerialBaud = 19200;                        // Baudrate für PC Serial Baud
-static const uint32_t SerialBaudGSM = 9600;                           // Baudrate Sim900 Modul
+static const uint32_t SerialBaud = 115200;              // Baudrate für PC Serial Baud
+static const uint32_t SerialBaudGSM = 9600;             // Baudrate Sim900 Modul
 static const uint32_t SerialBaudGPS = 9600;
-bool simm900_INIT = false;
-bool Neo6mGPS_Init = false;
+bool simm900_isINIT = false;
+bool Neo6mGPS_isInit = false;
+
+// Pinbelegungen
+const int Relay = 10;                   // Pin für Relays Standheizung an
+const int switchs = 7;                  // Pin für Stanheizung Starttaster
+const int esppin = 6;                   // Heizung Start
+const int EspPinRueck = 5;              // Rückmeldung an ESP
+const int HeizungLed = 8;               // Led Standheizung Aktiv
 
 
-bool InitSim900();
+void InitSim900();
 
 // the setup function runs once when you press reset or power the boardd
 void setup() 
@@ -24,19 +29,47 @@ void setup()
 	Serial.begin(SerialBaud);
 	Sim900_Serial.begin(SerialBaudGSM);
     GpsNeo6_Serial.begin(SerialBaudGPS);
+	Serial.println("booting.....");
+	// Sim Modul 10 Sec Zeit geben
+	delay(15000);
+	// AT an SIm900 Modul um Autobaudrate einzustellen
+	Serial.println("+++ sende AT fuer Autobaud +++");
+	while (Sim900_Serial.read() >= 0);  // Empfangsbuffer leeren
+	Sim900_Serial.println("AT");
+	delay(300);
+	while (Sim900_Serial.available())
+	{
+		Serial.write(Sim900_Serial.read());    //Forward what Software Serial received to Serial Port
+	}
+	delay(300);
+	Serial.println("wait for GSM init......");
 }
+
+
 
 // the loop function runs over and over again until power down or reset
 void loop() 
 {
-  
-	if ((millis() > 10000) and (simm900_INIT == false))
+	
+	// 10000 ms warten bis Modul eingebucht hat dann Initialisieren.
+	if ((millis() > 10000) && (simm900_isINIT == false))
 	{
+		Serial.println("loop");
 		Serial.println(millis());
 		InitSim900();
 		// InitGPSModul();
 	}
 
+	if (Sim900_Serial.available())
+		Serial.write(Sim900_Serial.read());
+	if (Serial.available())
+		Sim900_Serial.write(Serial.read());
+
+
+
+
+
+
 }
 
 
@@ -44,26 +77,29 @@ void loop()
 
 
 
-bool InitSim900()
+void InitSim900()
 {
-	simm900_INIT = false;
-	Serial.println("+++ Begin Setup +++");
-	delay(120);
+	simm900_isINIT = false;
+	Serial.println("+++ begin sim900 initialisierung +++");
+	delay(3000);
+	
+	
 	Sim900_Serial.println("AT");
-	delay(140);
+	delay(200);
 	while (Sim900_Serial.available())
 	{
 		Serial.write(Sim900_Serial.read());//Forward what Software Serial received to Serial Port
 	}
+	Serial.println("+++ Signal Qualitaet +++ abfragen");
 	Sim900_Serial.println("AT+CSQ"); //Signal quality test, value range is 0-31 , 31 is the best
-	delay(140);
+	delay(240);
 	while (Sim900_Serial.available())
 	{
 		Serial.write(Sim900_Serial.read());//Forward what Software Serial received to Serial Port
 	}
 
 	Sim900_Serial.println("AT+CCID"); //Read SIM information to confirm whether the SIM is plugged
-	delay(140);
+	delay(240);
 	while (Sim900_Serial.available())
 	{
 		Serial.write(Sim900_Serial.read());//Forward what Software Serial received to Serial Port
@@ -71,42 +107,45 @@ bool InitSim900()
 
 
 	Sim900_Serial.println("AT+CREG?"); //Check whether it has registered in the network
-	delay(130);
+	delay(240);
 	while (Sim900_Serial.available())
 	{
 		Serial.write(Sim900_Serial.read());//Forward what Software Serial received to Serial Port
 	}
 
 	Sim900_Serial.println("AT+CLIP=1"); //rufnummer anzeige aktivieren CLIP
-	delay(130);
+	delay(240);
 	while (Sim900_Serial.available())
 	{
 		Serial.write(Sim900_Serial.read());//Forward what Software Serial received to Serial Port
 	}
 
 	Sim900_Serial.println("AT+CNMI=2,2,0,0,0");
-	delay(140);
+	delay(240);
 	while (Sim900_Serial.available())
 	{
 		Serial.write(Sim900_Serial.read());//Forward what Software Serial received to Serial Port
 	}
 
 
+	Serial.println("set SMS mode to text");
 	Sim900_Serial.println("AT+CMGF=1"); // set SMS mode to text
-	delay(130);
+	delay(240);
 	while (Sim900_Serial.available())
 	{
 		Serial.write(Sim900_Serial.read());//Forward what Software Serial received to Serial Port
 	}
-
+	Serial.println("sms speicher leeren");
 	Sim900_Serial.println("AT+CMGD=1,4"); // delete all SMS
-	Serial.println("+++ All SMS geloescht +++");
-	delay(130);
+	Serial.println("+++ sms speicher leer +++");
+	delay(240);
 	while (Sim900_Serial.available())
 	{
 		Serial.write(Sim900_Serial.read());//Forward what Software Serial received to Serial Port
 	}
 
-	Serial.println("+++ Ende Setup +++");
-	return NULL;
+	Serial.println("+++ sim900 initialisiert +++");
+	
+
+	simm900_isINIT = true;
 }
